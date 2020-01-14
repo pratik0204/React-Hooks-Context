e3e17660e19c9acb15d6d4c6d259250f02f14a1c
pratik0204 <pchy.393@gmail.com>
2020-01-14 23:49:56 +0530

    consumer approach of accessing context

--- src/Components/booklist.js
+++ src/Components/booklist.js
@@ -2,18 +2,26 @@ import React, { Component } from 'react';
 import {ThemeContext} from '../Context/ThemeContext';
 class Booklist extends Component {
     state = {  }
-    static contextType= ThemeContext;
     render() { 
-        const {isLightTheme,light,dark}=this.context;
-        const theme = isLightTheme?light:dark;
+        
         return ( 
-            <div className="book-list"style={{color: theme.syntax,background:theme.bg}} >
-                <ul>
-                    <li style={{background: theme.ui}}>The way of kings</li>
-                    <li style={{background: theme.ui}}>Learning c++</li>
-                    <li style={{background: theme.ui}}>Object Oriented</li>
-                </ul>
-            </div>
+            <ThemeContext.Consumer>
+                {(context) => {
+
+                    const {isLightTheme,light,dark}=context;
+                    const theme = isLightTheme?light:dark;
+
+                    return(
+                        <div className="book-list"style={{color: theme.syntax,background:theme.bg}} >
+                            <ul>
+                                <li style={{background: theme.ui}}>The way of kings</li>
+                                <li style={{background: theme.ui}}>Learning c++</li>
+                                <li style={{background: theme.ui}}>Object Oriented</li>
+                            </ul>
+                        </div>
+                    )
+                }}
+            </ThemeContext.Consumer>
          );
     }
 }

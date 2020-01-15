31ec4f0c90ffca08873f1e1ef19c283fdcbaf56a
pratik0204 <pchy.393@gmail.com>
2020-01-15 17:36:09 +0530

    using useEffect which runs on every render

--- src/Components/SongList.js
+++ src/Components/SongList.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import uuid from 'uuid/v1';
 import NewSongForm from './NewSongForm';
 
@@ -9,9 +9,15 @@ const SongList = () => {
         {title : 'this wild darkness',id:3}
     ]);
 
+    const[age,setAge]=useState(20);
+
     const addSong =(title)=>{
         setSongs([ ...songs,{title:title,id:uuid()}])
     }
+
+    useEffect(()=>{
+        console.log('useEffect hook run',songs);
+    },[songs])
     return ( 
         <div className="song-list">
             <ul>
@@ -21,6 +27,7 @@ const SongList = () => {
             </ul>
 
             <NewSongForm addSong={addSong}/>
+            <button  onClick={()=> setAge(age+1)}>Add 1 to age:{age}</button>
         </div>
      );
 }
